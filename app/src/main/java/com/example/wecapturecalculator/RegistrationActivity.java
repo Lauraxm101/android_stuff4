fdd426848d8cc5cb3afc83e0b621f2cc32ae513b
Lauraxm101 <laura.juozapaityte@gmail.com>
2019-03-06 23:07:18 +0200

    Initial commit

--- app/src/main/java/com/example/wecapturecalculator/RegistrationActivity.java
+++ app/src/main/java/com/example/wecapturecalculator/RegistrationActivity.java
@@ -1,7 +1,12 @@
 package com.example.wecapturecalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText username=findViewById(R.id.username);
+        final EditText password=findViewById(R.id.password);
+        final EditText email=findViewById(R.id.email);
+
+        Button registration=findViewById(R.id.registration);
+
+        registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User user = new User(username.getText().toString(), email.getText().toString(), password.getText().toString());
+                Toast.makeText(RegistrationActivity.this,
+                        user.getUsername()+"\n"+user.getEmail()+"\n"+user.getPassword(),
+                        Toast.LENGTH_SHORT).show();
+                //                                                  Is kur               I kur
+                Intent goToSearchActivity = new Intent(RegistrationActivity.this, SearchActivity.class);
+                startActivity(goToSearchActivity);
+            }
+        });
     }
 }
